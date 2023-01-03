8c76ec91863ef2d25c9a08f5e4ae9bbe5ea2b051
Sergio Nieto <snieto10@gmail.com>
2023-01-02 19:38:58 -0500

    modify image

--- src/layout/TopTitle.jsx
+++ src/layout/TopTitle.jsx
@@ -1,14 +1,11 @@
 import github from "../assets/images/github.svg";
 import linkedIn from "../assets/images/linkedin.svg";
-import logo from "../assets/images/logo.svg";
 
 function TopTitle() {
   return (
     <>
       <div className="flex flex-spaceBetween max-width">
-        <div className="flex flex-logo">
-          <img src={logo} alt="sergioLogo" />
-        </div>
+        <div className="flex flex-logo">sergio</div>
         <div className="flex programming-lenguages opacity7 letter-space">
           <h3>JAVASCRIPT</h3>
           <h3>REACT</h3>
